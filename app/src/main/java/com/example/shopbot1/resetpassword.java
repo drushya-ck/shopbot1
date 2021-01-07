68c4c7c9c7514817cbdba7712468422668bc0c97
drushya-ck <drushya.is17@bmsce.ac.in>
2021-01-08 02:40:03 +0530

    Added files

--- app/src/main/java/com/example/shopbot1/resetpassword.java
+++ app/src/main/java/com/example/shopbot1/resetpassword.java
@@ -2,12 +2,26 @@ package com.example.shopbot1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class resetpassword extends AppCompatActivity {
     private TextView goback;
+    private Button reset;
+    FirebaseAuth auth;
+    AutoCompleteTextView email;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +33,26 @@ public class resetpassword extends AppCompatActivity {
                 startActivity(new Intent(resetpassword.this, login.class));
             }
         });
+        email=(AutoCompleteTextView) findViewById(R.id.atvEmailRes);
+
+        reset=(Button) findViewById(R.id.btnReset);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth =FirebaseAuth.getInstance();
+//        var emailAddress = "user@example.com";
+
+                auth.sendPasswordResetEmail(email.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(resetpassword.this, "Email sent.",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
     }
 }
